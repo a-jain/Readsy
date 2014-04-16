efa54e57dd58cf2f0c8aecbf0f6b7e5084471486
Akash Jain <mr.akashj@gmail.com>
2014-04-16 19:52:50 -0400

    clarify

--- static/js/app.js
+++ static/js/app.js
@@ -14,9 +14,12 @@
     }
     
     function fillTextClick() {
-    	var testText = "Spritzing content from the web is also easy. All you need to do is publish content on the web, "
-    		+ " and our platform can transform it into Spritz Text. The default configuration simply extracts all text " 
-    		+ "from the HTML at the target URL, but advanced configuration allows finer control using CSS-like selectors.";    		
+    	var testText = "Here's To The Crazy Ones. The misfits. The rebels. The trouble-makers. The round pegs in the
+square holes. The ones who see things differently. They're not fond of rules, and they have
+no respect for the status-quo. You can quote them, disagree with them, glorify, or vilify them.
+About the only thing you can't do is ignore them. Because they change things. They push the
+human race forward. And while some may see them as the crazy ones, we see genius.
+Because the people who are crazy enough to think they can change the world - are the ones who do";    		
     	var text = $("#inputText").val();   	
         $("#inputText").val(text + (text != "" ? "\n" : "") + testText);
     }
@@ -35,7 +38,7 @@
 			"redicleWidth" : 	434,	// Specify Redicle width
 			"redicleHeight" : 	150,	// Specify Redicle height
 			"defaultSpeed" : 600,
-			"speedItems" : [150, 200, 300, 400, 450, 500, 600, 750, 1000] 
+			"speedItems" : [150, 200, 300, 400, 450, 500, 550, 600, 750] 
 			
 	};
 
