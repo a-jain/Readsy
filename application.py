68a0452ec628b6a62d6bc43d8c41232f005b064f
Akash Jain <mr.akashj@gmail.com>
2014-04-16 11:25:13 -0400

    css

--- application.py
+++ application.py
@@ -2,15 +2,7 @@ import os.path
 from flask import Flask, render_template, flash, url_for, request, redirect, abort
 from flask import send_from_directory
 from werkzeug.utils import secure_filename
-import MySQLdb
 import json
-
-application = Flask(__name__)
-application.debug = True
-application.secret_key = '\x99\x02~p\x90\xa3\xce~\xe0\xe6Q\xe3\x8c\xac\xe9\x94\x84B\xe7\x9d=\xdf\xbb&'
-
-####################################################### 
-
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
@@ -22,10 +14,16 @@ import HTMLParser
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+application = Flask(__name__)
+application.debug = True
+application.secret_key = '\x99\x02~p\x90\xa3\xce~\xe0\xe6Q\xe3\x8c\xac\xe9\x94\x84B\xe7\x9d=\xdf\xbb&'
+
 UPLOAD_FOLDER = 'static/spritz/'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf'])
 application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 application.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
+
+####################################################### 
 
 def convert_pdf_to_txt(path):
 	rsrcmgr = PDFResourceManager()
@@ -47,6 +45,11 @@ def convert_pdf_to_txt(path):
 	retstr.close()
 	return str
 
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+####################################################### 
+
 @application.route('/')
 @application.route('/spritzy/<filename>')
 def spritz(filename=None):
@@ -59,20 +62,17 @@ def spritz(filename=None):
 	s = re.sub(r'\s+', ' ', s)
 	s = s.replace('!', '')
 	s = HTMLParser.HTMLParser().unescape(s)
-	# s = unicode(s, errors='replace')
-	# s = s.decode('utf8').encode('ascii')
 	# print s
 	
 	return render_template('spritz.html', text=s) 
+
 
 @application.route('/spritz/login_success')
 def spritz_login():
 	return render_template('login_success.html')
 
-def allowed_file(filename):
-	return '.' in filename and \
-		   filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+@application.route('/upload')
 @application.route('/upload', methods=['GET', 'POST'])
 def upload_file():
 	if request.method == 'POST':
@@ -84,13 +84,14 @@ def upload_file():
 			# return redirect(url_for('uploaded_file', filename=filename))
 	return render_template('upload_form.html')
 
+
 @application.route('/uploads/<filename>')
 def uploaded_file(filename):
 	if not filename:
 		return redirect(url_for('uploaded_file', filename=filename))
 
-	return send_from_directory(application.config['UPLOAD_FOLDER'],
-							   filename)
+	return send_from_directory(application.config['UPLOAD_FOLDER'], filename)
+	
 
 if __name__ == '__main__':
 	application.run(debug=True)
