ddaf81236f710b5879f135cb2e51f0e36e411838
Akash Jain <mr.akashj@gmail.com>
2014-04-16 10:34:24 -0400

    helperform

--- application.py
+++ application.py
@@ -28,75 +28,69 @@ application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 application.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
 
 def convert_pdf_to_txt(path):
-    rsrcmgr = PDFResourceManager()
-    retstr = StringIO()
-    codec = 'utf-8'
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-    fp = file(path, 'rb')
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    password = ""
-    maxpages = 0
-    caching = True
-    pagenos=set()
-    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
-        interpreter.process_page(page)
-    fp.close()
-    device.close()
-    str = retstr.getvalue()
-    retstr.close()
-    return str
+	rsrcmgr = PDFResourceManager()
+	retstr = StringIO()
+	codec = 'utf-8'
+	laparams = LAParams()
+	device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+	fp = file(path, 'rb')
+	interpreter = PDFPageInterpreter(rsrcmgr, device)
+	password = ""
+	maxpages = 0
+	caching = True
+	pagenos=set()
+	for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+		interpreter.process_page(page)
+	fp.close()
+	device.close()
+	str = retstr.getvalue()
+	retstr.close()
+	return str
 
 @application.route('/')
-@application.route('/spritzme/<filename>')
+@application.route('/spritzy/<filename>')
 def spritz(filename=None):
 	if not filename:
 		return render_template('spritz.html', text="")
 
-	else:
-		url = "static/spritz/" + filename
-		print url
-		s = convert_pdf_to_txt(url)
-		s = re.sub(r'\s+', ' ', s)
-		s = s.replace('!', '')
-		s = HTMLParser.HTMLParser().unescape(s)
-		# s = unicode(s, errors='replace')
-		# s = s.decode('utf8').encode('ascii')
-		# print s
-		
-		return render_template('spritz.html', text=s) 
+	url = "static/spritz/" + filename
+	print url
+	s = convert_pdf_to_txt(url)
+	s = re.sub(r'\s+', ' ', s)
+	s = s.replace('!', '')
+	s = HTMLParser.HTMLParser().unescape(s)
+	# s = unicode(s, errors='replace')
+	# s = s.decode('utf8').encode('ascii')
+	# print s
+	
+	return render_template('spritz.html', text=s) 
 
 @application.route('/spritz/login_success')
 def spritz_login():
 	return render_template('login_success.html')
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+	return '.' in filename and \
+		   filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@application.route('/spritz/upload', methods=['GET', 'POST'])
+@application.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(application.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('spritz', filename=filename))
-            # return redirect(url_for('uploaded_file', filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload File to be Spritzed</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    ''' 
+	if request.method == 'POST':
+		file = request.files['file']
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(application.config['UPLOAD_FOLDER'], filename))
+			return redirect(url_for('spritz', filename=filename))
+			# return redirect(url_for('uploaded_file', filename=filename))
+	return render_template('upload_form.html')
 
-@application.route('/spritz/uploads/<filename>')
+@application.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(application.config['UPLOAD_FOLDER'],
-                               filename)
+	if not filename:
+		return redirect(url_for('uploaded_file', filename=filename))
+
+	return send_from_directory(application.config['UPLOAD_FOLDER'],
+							   filename)
 
 if __name__ == '__main__':
 	application.run(debug=True)
