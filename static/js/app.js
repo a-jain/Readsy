7b91538b06fe004133abf0445486c6ac36a62c91
Akash Jain <mr.akashj@gmail.com>
2014-04-16 20:02:42 -0400

    clarify

--- static/js/app.js
+++ static/js/app.js
@@ -14,12 +14,7 @@
     }
     
     function fillTextClick() {
-    	var testText = "Here's To The Crazy Ones. The misfits. The rebels. The trouble-makers. The round pegs in the
-square holes. The ones who see things differently. They're not fond of rules, and they have
-no respect for the status-quo. You can quote them, disagree with them, glorify, or vilify them.
-About the only thing you can't do is ignore them. Because they change things. They push the
-human race forward. And while some may see them as the crazy ones, we see genius.
-Because the people who are crazy enough to think they can change the world - are the ones who do";    		
+    	var testText = "Here's To The Crazy Ones. The misfits. The rebels. The trouble-makers. The round pegs in the square holes. The ones who see things differently. They're not fond of rules, and they have no respect for the status-quo. You can quote them, disagree with them, glorify, or vilify them. About the only thing you can't do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world - are the ones who do";    		
     	var text = $("#inputText").val();   	
         $("#inputText").val(text + (text != "" ? "\n" : "") + testText);
     }
