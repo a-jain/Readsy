bb7a95235ad7f4d7e1ce8d40a34f3b94a1e13a77
Akash Jain <mr.akashj@gmail.com>
2014-04-18 14:06:49 -0400

    webform

--- application.py
+++ application.py
@@ -179,24 +179,19 @@ def url_handle():
 
 	if not url or url == '':
 		return redirect(url_for('index'))
-
-	print url
 	
 	if '://' not in url:
 		url = "http://" + url
 
-	print url
 	READABILITY_TOKEN = 'd58d28ee3b6259ece0a6f7b3ad985aa171fe8ac5'
 	parser_client = ParserClient(READABILITY_TOKEN)
 
 	parser_response = parser_client.get_article_content(url)
-	print parser_response
 	contentStr = parser_response.content['title'] + parser_response.content['content']
 
 	soup = BeautifulSoup(contentStr)
 	s = soup.get_text()
 	s = re.sub(r'\s+', ' ', s)
-	print s
 
 	return render_template('spritz.html', text=s)
 
