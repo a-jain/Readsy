00d9ead29327376ff815bec4465a4f18a7462918
Akash Jain <mr.akashj@gmail.com>
2014-04-18 00:34:09 -0400

    urlhandling

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, url_for, request, redirect, abort
-from flask import send_from_directory, safe_join
+from flask import send_from_directory, safe_join, json
 from werkzeug.utils import secure_filename
 from hashlib import sha1
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
@@ -8,6 +8,9 @@ from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfparser import PDFSyntaxError
 from cStringIO import StringIO
+from readability import ParserClient
+from urlparse import urlparse
+from bs4 import BeautifulSoup
 # from boto.s3.connection import S3Connection
 # from boto.s3.key import Key
 
@@ -16,7 +19,7 @@ from cStringIO import StringIO
 # key = bucket.get_key("picture.jpg")
 # fp = open ("picture.jpg", "w")
 # key.get_file (fp)
-import re, sys, json, os, base64, hmac, urllib, time
+import re, sys, os, base64, hmac, urllib, time
 import HTMLParser
 reload(sys)
 sys.setdefaultencoding("utf-8")
@@ -70,6 +73,34 @@ def page_not_found(error):
 @application.errorhandler(500)
 def special_exception_handler(error):
     return 'File doesn\'t exist any more :(', 500
+	
+@application.route('/spritz/login_success')
+def spritz_login():
+	return render_template('login_success.html')
+
+@application.route('/sign_s3/')
+def sign_s3():
+    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    S3_BUCKET = os.environ.get('S3_BUCKET')
+
+    object_name = request.args.get('s3_object_name')
+    mime_type = request.args.get('s3_object_type')
+
+    expires = int(time.time()+10)
+    amz_headers = "x-amz-acl:public-read"
+
+    put_request = "PUT\n\n%s\n%d\n%s\n/%s/%s" % (mime_type, expires, amz_headers, S3_BUCKET, object_name)
+
+    signature = base64.encodestring(hmac.new(AWS_SECRET_KEY, put_request, sha1).digest())
+    signature = urllib.quote_plus(signature.strip())
+
+    url = 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, object_name)
+
+    return json.dumps({
+        'signed_request': '%s?AWSAccessKeyId=%s&Expires=%d&Signature=%s' % (url, AWS_ACCESS_KEY, expires, signature),
+         'url': url
+      })
 
 #######################################################
 
@@ -121,11 +152,6 @@ def spritz(filename=None):
 
 	return redirect(url_for('index'))
 	# print s
-	
-@application.route('/spritz/login_success')
-def spritz_login():
-	return render_template('login_success.html')
-
 
 @application.route('/upload')
 @application.route('/upload', methods=['GET', 'POST'])
@@ -147,30 +173,32 @@ def uploaded_file(filename):
 
 	return send_from_directory(application.config['UPLOAD_FOLDER'], filename)
 
-@application.route('/sign_s3/')
-def sign_s3():
-    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    S3_BUCKET = os.environ.get('S3_BUCKET')
+@application.route('/web')
+def url_handle():
+	url = request.args.get('url', '')
 
-    object_name = request.args.get('s3_object_name')
-    mime_type = request.args.get('s3_object_type')
+	if not url or url == '':
+		return redirect(url_for('index'))
 
-    expires = int(time.time()+10)
-    amz_headers = "x-amz-acl:public-read"
+	print url
+	
+	if '://' not in url:
+		url = "http://" + url
 
-    put_request = "PUT\n\n%s\n%d\n%s\n/%s/%s" % (mime_type, expires, amz_headers, S3_BUCKET, object_name)
+	print url
+	READABILITY_TOKEN = 'd58d28ee3b6259ece0a6f7b3ad985aa171fe8ac5'
+	parser_client = ParserClient(READABILITY_TOKEN)
 
-    signature = base64.encodestring(hmac.new(AWS_SECRET_KEY, put_request, sha1).digest())
-    signature = urllib.quote_plus(signature.strip())
+	parser_response = parser_client.get_article_content(url)
+	print parser_response
+	contentStr = parser_response.content['title'] + parser_response.content['content']
 
-    url = 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, object_name)
+	soup = BeautifulSoup(contentStr)
+	s = soup.get_text()
+	s = re.sub(r'\s+', ' ', s)
+	print s
 
-    return json.dumps({
-        'signed_request': '%s?AWSAccessKeyId=%s&Expires=%d&Signature=%s' % (url, AWS_ACCESS_KEY, expires, signature),
-         'url': url
-      })
-
+	return render_template('spritz.html', text=s)
 
 if __name__ == '__main__':
 	application.run(debug=True)
