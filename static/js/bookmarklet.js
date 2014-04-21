c597cf0583866876f2f7cf1d9817d59f31252bcb
Akash Jain <mr.akashj@gmail.com>
2014-04-20 22:50:51 -0400

    bookmarklet

--- static/js/bookmarklet.js
+++ static/js/bookmarklet.js
@@ -1,1 +1,3 @@
-javascript: window.location = "www.readsy.co/web?url=" + window.location.host + "/" + window.location.pathname;
+href='javascript:void((function(){var a=location.href.replace(/^http%5C:%5C/%5C/(.*)$/,"$1");location.href="http://www.readsy.co/web?url="+escape(a);})())'
+
+onclick="alert('Drag this link onto your toolbar to add the Readsy bookmarklet');return false"
