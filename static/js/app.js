6c5f23f096fbd1735b38076d5f1955085b5b27d3
Akash Jain <mr.akashj@gmail.com>
2014-05-14 19:33:45 -0400

    formatting

--- static/js/app.js
+++ static/js/app.js
@@ -44,7 +44,7 @@
 			"redicleWidth" : 	434,	// Specify Redicle width
 			"redicleHeight" : 	135,	// Specify Redicle height
 			"defaultSpeed" :    300,
-			"speedItems" :      [250, 300, 350, 400, 450, 500, 550, 600, 950],
+			"speedItems" :      [50, 100, 150, 200, 250, 300, 400, 500, 600, 900],
 			"controlTitles" : {
             "pause" :         "Pause",
             "play" :          "Play",
