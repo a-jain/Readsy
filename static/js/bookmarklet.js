b4a155d67a7b13930ae1b166ba0ffe89a6abd820
Akash Jain <mr.akashj@gmail.com>
2014-05-15 21:06:19 -0400

    bookmarklet

--- static/js/bookmarklet.js
+++ static/js/bookmarklet.js
@@ -12,7 +12,7 @@ javascript:void(
 			var a=location.href.replace(/^http%5C:%5C/%5C/(.*)$/,"$1");
 			location.href="http://www.readsy.co/web?url="+encodeURIComponent(a);
 		} else {
-			location.href="http://www.readsy.co/text/"+encodeURIComponent(t.replace("/", "%2F"));
+			location.href="http://www.readsy.co/text/"+encodeURIComponent(t.replace("/", " "));
 		}
 	})
 	()
@@ -20,4 +20,4 @@ javascript:void(
 
 document.URL.split("/")[3]
 
-javascript:void((function(){var t = ""; if(window.getSelection){t = window.getSelection();}else if(document.getSelection){t = document.getSelection();}else if(document.selection){	t = document.selection.createRange().text;}	if (t == "") {var a=location.href.replace(/^http%5C:%5C/%5C/(.*)$/,"$1");location.href="http://www.readsy.co/web?url="+encodeURIComponent(a);} else {location.href="http://www.readsy.co/text/"+encodeURIComponent(t.replace("/", "%2F"));}})())
+javascript:void((function(){var t = ""; if(window.getSelection){t = window.getSelection();}else if(document.getSelection){t = document.getSelection();}else if(document.selection){	t = document.selection.createRange().text;}	if (t == "") {var a=location.href.replace(/^http%5C:%5C/%5C/(.*)$/,"$1");location.href="http://www.readsy.co/web?url="+encodeURIComponent(a);} else {location.href="http://www.readsy.co/text/"+encodeURIComponent(t.replace("/", " "));}})())
