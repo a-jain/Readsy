1f84f105524f0e0c90fabfb066603da3b791eeee
Akash Jain <mr.akashj@gmail.com>
2014-05-15 21:35:07 -0400

    bookmarklet

--- static/js/bookmarklet.js
+++ static/js/bookmarklet.js
@@ -1,23 +1,23 @@
 javascript:void(
-	(function() {
-		var t = ""';
-		if(window.getSelection){
-			t = window.getSelection();
-		}else if(document.getSelection){
-			t = document.getSelection();
-		}else if(document.selection){
-			t = document.selection.createRange().text;
-		}
-		if (t == '') {
-			var a=location.href.replace(/^http%5C:%5C/%5C/(.*)$/,"$1");
-			location.href="http://www.readsy.co/web?url="+encodeURIComponent(a);
-		} else {
-			location.href="http://www.readsy.co/text/"+encodeURIComponent(t.replace("/", " "));
-		}
-	})
-	()
+  (function() {
+    var t = "";
+    if(window.getSelection){
+      t = window.getSelection();
+    }else if(document.getSelection){
+      t = document.getSelection();
+    }else if(document.selection){
+      t = document.selection.createRange().text;
+    }
+    if (t == "") {
+      var a=location.href.replace(/^http%5C:%5C/%5C/(.*)$/,"$1");
+      location.href="http://www.readsy.co/web?url="+encodeURIComponent(a);
+    } else {
+      location.href="http://www.readsy.co/text/"+encodeURIComponent(t).replace(/[\/]/g, "%20");
+    }
+  })
+  ()
 )
 
 document.URL.split("/")[3]
 
-javascript:void((function(){var t = ""; if(window.getSelection){t = window.getSelection();}else if(document.getSelection){t = document.getSelection();}else if(document.selection){	t = document.selection.createRange().text;}	if (t == "") {var a=location.href.replace(/^http%5C:%5C/%5C/(.*)$/,"$1");location.href="http://www.readsy.co/web?url="+encodeURIComponent(a);} else {location.href="http://www.readsy.co/text/"+encodeURIComponent(t.replace("/", " "));}})())
+javascript:void((function() {var t="";if(window.getSelection){t=window.getSelection();}elseif(document.getSelection){t=document.getSelection();}else if(document.selection){t=document.selection.createRange().text;}if (t==""){var a=location.href.replace(/^http%5C:%5C/%5C/(.*)$/,"$1");location.href="http://www.readsy.co/web?url="+encodeURIComponent(a);} else {location.href="http://www.readsy.co/text/"+encodeURIComponent(t).replace(/[\/]/g,"%20");}})())
