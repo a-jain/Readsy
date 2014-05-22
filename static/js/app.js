0f0d8b41adb20f82abe51718c6c2bc72ce36600c
Akash Jain <mr.akashj@gmail.com>
2014-05-22 17:37:47 -0400

    footerposition

--- static/js/app.js
+++ static/js/app.js
@@ -33,7 +33,7 @@
 	};
 
 
-	// Display "Completed X of XXX"
+	// Display "Completed X of xxx"
 	function  showProgress(completed, total) {
 	     $("#wordNumber").text(completed);
 	     $("#wordTotal").text(total);
