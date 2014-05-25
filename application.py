f6ab663827e0ca85e31b472466a09b2f0d4de36a
Akash Jain <mr.akashj@gmail.com>
2014-05-25 23:44:54 +0100

    fonts

--- application.py
+++ application.py
@@ -36,7 +36,7 @@ def start_app():
 	assets = Environment()	
 	# use closure_js once i have java 7
 	js = Bundle('js/jquery.cookie.js', 'js/app.js', 'js/froala_editor.min.js', filters='rjsmin', output='gen/packed.js')
-	css = Bundle('css/bootstrap.min.css', 'css/bootstrapcustom.css', 'css/froala_editor.min.css', filters='cssmin', output='gen/packed.css')
+	css = Bundle('css/bootstrap.min.css', 'css/froala_editor.min.css', 'css/bootstrapcustom.css', filters='cssmin', output='gen/packed.css')
 	assets.register('js_all', js)
 	assets.register('css_all', css)
 	app.config['ASSETS_DEBUG'] = False
